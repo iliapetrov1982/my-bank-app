d0b3b67695144c5cdc6a4639cdaa2e54a268b5a6
iliapetrov <petrovilia8@gmail.com>
2026-04-19 18:38:02 +0200

    sprint 10 | remove Eureka and Config Server, parameterize service URLs
    - Remove spring-cloud-starter-netflix-eureka-client from all services
    - Remove spring-cloud-starter-config from all services
    - Remove eureka config block from notifications application.yml
    - Remove config server import from all application.yml files
    - Add jwk-set-uri to cash and transfer (previously provided by Config Server)
    - Change gateway routes from lb:// to direct http:// with env var support
    - Parameterize all service URLs and Keycloak URIs via environment variables
    - Add spring-boot-starter-json to fix Jackson 3.x transitive dependency
    - Update test imports from com.fasterxml to tools.jackson (Jackson 3.x)

--- accounts/src/test/java/ru/yandex/practicum/accounts/controller/AccountControllerTest.java
+++ accounts/src/test/java/ru/yandex/practicum/accounts/controller/AccountControllerTest.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.accounts.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
